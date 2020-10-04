fb93eb9656ef56bd7d49501a12d8eb38e40f0d29
jaskanwar <jaskanwar.singh1@gmail.com>
2020-10-04 19:41:25 -0400

    Checked and fixed validation errors.

--- pokemonSearch.js
+++ pokemonSearch.js
@@ -17,11 +17,12 @@ function nameSearch() {
     li = ul.getElementsByTagName("li"); //Gets all the li elements within the UL
     newDiv = document.getElementById("myDiv")
 
+    //While loop runs well there is a child and deletes all the previous so newest results are displayed
     while (newDiv.firstChild) {
         newDiv.removeChild(newDiv.lastChild);
     }
-    // Loop through all list items, and match search
 
+    // Loop through all list items, and match search
     for (i = 0; i < li.length; i++) {
         a = li[i].getElementsByTagName("a")[0]; //gets the name elements using the tag assigned for names
         txtValue = a.textContent || a.innerText; //gets the text inside the tag
@@ -42,13 +43,14 @@ function numSearch() {
     li = ul.getElementsByTagName("li"); //Gets all the li elements within the UL
     newDiv =document.getElementById("myDiv")
 
+    //While loop runs well there is a child and deletes all the previous so newest results are displayed
     while (newDiv.firstChild) {
         newDiv.removeChild(newDiv.lastChild);
     }
 
     // Loop through all list items, and match search
     for (i = 0; i < li.length; i++) {
-        c = li[i].getElementsByTagName("c")[0]; //gets the name elements using the tag assigned for names
+        c = li[i].getElementsByClassName("c")[0]; //gets the name elements using the tag assigned for names
         txtValue = c.textContent || c.innerText; //gets the text inside the tag
         if (txtValue.toUpperCase().indexOf(filter) > -1 && filter.length >0) { //checks to see if the name inputted matches any in the pokedex and counts to 5
             let listItem = li[i].cloneNode(true);
