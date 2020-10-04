f958f076f1cbb59554cbcae7995c8e6c3ec3f4c2
jaskanwar <jaskanwar.singh1@gmail.com>
2020-10-04 18:31:25 -0400

    Implemented both number and name search.

--- pokemonSearch.js
+++ pokemonSearch.js
@@ -1,53 +1,59 @@
+function createDiv(){
+    let div1 = document.getElementById("div1");
+    let div2 = document.createElement("div");
+    let ul = document.createElement("ul");
+    ul.id = "myDiv";
+
+    div2.appendChild(ul);
+    div1.appendChild(div2);
+}
+
 function nameSearch() {
     // Declare variables
-    let input, filter, ul, li, a, i, txtValue, newDiv, display=[];
+    let input, filter, ul, li, a, i, txtValue, newDiv;
     input = document.getElementById("nameSearch"); //gets the user input
     filter = input.value.toUpperCase();//Converts input to uppercase
     ul = document.getElementById("myUL"); //Gets all elements within the UL
     li = ul.getElementsByTagName("li"); //Gets all the li elements within the UL
-    newDiv = document.getElementById("myDiv");
+    newDiv = document.getElementById("myDiv")
 
-    // Loop through all list items, and match search
     while (newDiv.firstChild) {
-        newDiv.removeChild(newDiv.firstChild)
+        newDiv.removeChild(newDiv.lastChild);
     }
+    // Loop through all list items, and match search
+
     for (i = 0; i < li.length; i++) {
-        //newDiv.append(li[i])
         a = li[i].getElementsByTagName("a")[0]; //gets the name elements using the tag assigned for names
         txtValue = a.textContent || a.innerText; //gets the text inside the tag
-        if (txtValue.toUpperCase().indexOf(filter) > -1) { //checks to see if the name inputted matches any in the pokedex and counts to 5
-            //li[i].style.display = "";
-            newDiv.append(li[i].getElementsByTagName("a")[0].innerText);
-
-        } else {
-            //li[i].style.display = "none";
+        if (txtValue.toUpperCase().indexOf(filter) > -1 && filter.length >0) { //checks to see if the name inputted matches any in the pokedex and counts to 5
+            let listItem = li[i].cloneNode(true);
+            listItem.id = "goon";
+            newDiv.appendChild(listItem);
         }
     }
-    //alert(display);
 }
 
-function getNumInput(){
-    let input = document.getElementById("numSearch"); //gets the user input
-    numSearch(input);
-}
-
-function numSearch(input) {
+function numSearch() {
     // Declare variables
-    let filter, ul, li, c, i, txtValue, display = [], count = 0;
+    let input, filter, ul, li, c, i, txtValue, newDiv;
+    input = document.getElementById("numSearch"); //gets the user input
     filter = input.value.toUpperCase(); //Converts input to uppercase
     ul = document.getElementById("myUL"); //Gets all elements within the UL
     li = ul.getElementsByTagName("li"); //Gets all the li elements within the UL
+    newDiv =document.getElementById("myDiv")
+
+    while (newDiv.firstChild) {
+        newDiv.removeChild(newDiv.lastChild);
+    }
 
     // Loop through all list items, and match search
     for (i = 0; i < li.length; i++) {
         c = li[i].getElementsByTagName("c")[0]; //gets the name elements using the tag assigned for names
         txtValue = c.textContent || c.innerText; //gets the text inside the tag
-        if (txtValue.toUpperCase().indexOf(filter) > -1 && count < 5) { //checks to see if the name inputted matches any in the pokedex and counts to 5
-            count++; //count increases so the search can not exceed 5 results
-            display.push("\n"+li[i].getElementsByTagName("a")[0].innerText
-                + " " + li[i].getElementsByTagName("c")[0].innerText
-                + "\n" +li[i].getElementsByTagName("p")[0].innerText); //displays info in alert box
+        if (txtValue.toUpperCase().indexOf(filter) > -1 && filter.length >0) { //checks to see if the name inputted matches any in the pokedex and counts to 5
+            let listItem = li[i].cloneNode(true);
+            listItem.id = "goon";
+            newDiv.appendChild(listItem);
         }
     }
-    alert(display);
 }
