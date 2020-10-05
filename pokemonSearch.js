496c9da5975dc93c4047efbbbc5d95e1ec2b5c24
jaskanwar <jaskanwar.singh1@gmail.com>
2020-10-04 20:53:54 -0400

    Checked and fixed validation errors.

--- pokemonSearch.js
+++ pokemonSearch.js
@@ -40,7 +40,7 @@ function nameSearch() {
 
     // Loop through all list items, and match search
     for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0]; //gets the name elements using the tag assigned for names
+        a = li[i].getElementsByClassName("a")[0]; //gets the name elements using the tag assigned for names
         txtValue = a.textContent || a.innerText; //gets the text inside the tag
         if (txtValue.toUpperCase().indexOf(filter) > -1 && filter.length >0) { //checks to see if the name inputted matches any in the pokedex and counts to 5
             let listItem = li[i].cloneNode(true); //clones the filtered search into new variable
